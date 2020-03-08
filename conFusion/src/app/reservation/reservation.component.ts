daaadadbe929a1fb69b028e5513cb01061a1bb62
shilpakancharla <shilpakancharla18@gmail.com>
2020-03-08 14:03:00 -0400

    Assignment 3

--- conFusion/src/app/reservation/reservation.component.ts
+++ conFusion/src/app/reservation/reservation.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit, Inject, ChangeDetectorRef, ViewContainerRef } from '@angular/core';
-import { TextField } from 'ui/text-field';
-import { Switch } from 'ui/switch';
+import { TextField } from 'tns-core-modules/ui/text-field';
+import { Switch } from 'tns-core-modules/ui/switch';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { ModalDialogService, ModalDialogOptions, ModalDialogParams } from 'nativescript-angular/modal-dialog';
-import { RservationModalComponent, ReservationModalComponent } from "../reservationmodal/reservationmodal.component";
+import { ReservationModalComponent } from "../reservationmodal/reservationmodal.component";
+import { View, Page } from 'tns-core-modules/ui/page/page';
+import { CouchbaseService } from '../services/couchbase.service';
+import * as enums from 'tns-core-modules/ui/enums';
+import { RadSideDrawer } from 'nativescript-telerik-ui/sidedrawer';
+import * as app from "tns-core-modules/application";
 
 @Component({
     selector: 'app-reservation',
@@ -13,14 +18,28 @@ import { RservationModalComponent, ReservationModalComponent } from "../reservat
 })
 export class ReservationComponent implements OnInit {
     reservation: FormGroup;
+    showSubmission: boolean = false;
+    formView: View;
+    submissionView: View;
+    reservationSubmission: any;
+    reservations: Reservation[];
+    docId: string = 'reservations';
+
     constructor(private formBuilder: FormBuilder,
         private modalService: ModalDialogService,
+        private couchbaseService: CouchbaseService,
+        private page: Page,
         private vcRef: ViewContainerRef) {
         this.reservation = this.formBuilder.group({
             guests: 3,
             smoking: false,
             dateTime: ['', Validators.required]
         });
+        this.reservationSubmission = {
+            guests: 3,
+            smoking: 'No',
+            dateTime: ''
+        };
     }
 
     ngOnInit() { }
@@ -61,7 +80,54 @@ export class ReservationComponent implements OnInit {
         this.reservation.patchValue({ dateTime: textField.text });
     }
 
+    onDrawerButtonTap(): void {
+        const sideDrawer = <RadSideDrawer>app.getRootView();
+        sideDrawer.showDrawer();
+    }
+
     onSubmit() {
+        this.reservationSubmission = this.reservation.value;
+        let doc = this.couchbaseService.getDocument(this.docId);
+        if (doc == null) {
+            this.couchbaseService.createDocument(
+                { reservations: [this.reservationSubmission] },
+                this.docId
+            );
+        } else {
+            this.reservations = doc.reservations;
+            this.reservations.push(this.reservationSubmission);
+            this.couchbaseService.updateDocument(this.docId, {
+                reservations: this.reservations
+            });
+        }
+
+        if (this.reservationSubmission.smoking) {
+            this.reservationSubmission.smoking = 'Yes';
+        } else {
+            this.reservationSubmission.smoking = 'No';
+        }
+        this.animateForm();
         console.log(JSON.stringify(this.reservation.value));
+    }
+
+    animateForm() {
+        this.formView = this.page.getViewById<View>('formView');
+        this.formView.animate({
+            target: this.formView,
+            scale: { x: 0,  y: 0 },
+            translate: { x: 0, y: -2000 },
+            opacity: 1,
+            duration: 500,
+            curve: enums.AnimationCurve.easeIn
+        });
+        this.submissionView = this.page.getViewById<View>('submissionView');
+        this.submissionView.animate({
+            scale: { x: 1, y: 1 },
+            translate: { x: 0, y: 0 },
+            opacity: 1,
+            duration: 2500,
+            curve: enums.AnimationCurve.easeIn
+        });
+        this.showSubmission = true;
     }
 }
